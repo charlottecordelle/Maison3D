346f65a078fb01e59bd96dbaa259d18aa47f5e77
Charlotte Cordelle <charlottecordelle@Charlottes-MacBook-Pro-2.local>
2025-09-17 13:02:23 +0200

    Premiere rotation de la maison

--- Maison_3D/src/app/app.component.ts
+++ Maison_3D/src/app/app.component.ts
@@ -2,7 +2,6 @@ import { Component, ElementRef, ViewChild, OnInit, AfterViewInit } from '@angula
 import { RouterOutlet } from '@angular/router';
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/Addons.js';
-import { Group } from 'three/examples/jsm/libs/tween.module.js';
 
 @Component({
   selector: 'app-root',
@@ -38,45 +37,45 @@ export class AppComponent implements AfterViewInit {
     const longueur2 = 5
     const hauteur = 2.5;
 
-    const mur1Geo = new THREE.BoxGeometry(largeur, hauteur, longueur1);
+    const mur1Geo = new THREE.BoxGeometry(longueur1, hauteur, largeur);
     const mur1Material = new THREE.MeshBasicMaterial({ color: 'gray' });
     const mur1 = new THREE.Mesh(mur1Geo, mur1Material);
-    mur1.position.set(0, hauteur/2, 4);
+    mur1.position.set(0, hauteur/2, 2.5);
     houseGroup.add(mur1)
-
 
     const mur2Geo = new THREE.BoxGeometry(largeur, hauteur, longueur2);
     const mur2Material = new THREE.MeshBasicMaterial({ color: 'gray' })
     const mur2 = new THREE.Mesh(mur2Geo, mur2Material);
-    mur2.position.set(2.5, hauteur/2, 8);
+    mur2.position.set(-3.9, hauteur/2, 0);
     houseGroup.add(mur2)
 
     const mur3Geo = new THREE.BoxGeometry(longueur1, hauteur, largeur);
     const mur3Material = new THREE.MeshBasicMaterial({ color: 'gray' });
     const mur3 = new THREE.Mesh(mur3Geo, mur3Material);
-    mur3.position.set(5, hauteur/2, 0);
+    mur3.position.set(0, hauteur/2, -2.5);
     houseGroup.add(mur3)
 
-    const mur4Geo = new THREE.BoxGeometry(longueur2, hauteur, largeur);
+    const mur4Geo = new THREE.BoxGeometry(largeur, hauteur, longueur2);
     const mur4Material = new THREE.MeshBasicMaterial({ color:'gray' });
     const mur4 = new THREE.Mesh(mur4Geo, mur4Material);
-    mur4.position.set(2.5, hauteur/2, 0);
+    mur4.position.set(3.9, hauteur/2, 0);
     houseGroup.add(mur4)
 
     const largeursol = 5;
     const longueursol = 8;
     const epaisseur = 0.20;
+    const largeurplan = 6;
 
-    const solGeo = new THREE.BoxGeometry(largeursol, longueursol, epaisseur)
+    const solGeo = new THREE.BoxGeometry(largeursol, epaisseur, longueursol)
     const solMaterial = new THREE.MeshBasicMaterial({ color: 'beige'});
     const sol = new THREE.Mesh(solGeo, solMaterial);
-    sol.position.set(2.5, 4, -1.15)
+    sol.position.set(0, 0, 0)
     scene.add(sol)
 
-    const plancherGeo = new THREE.BoxGeometry(largeursol, longueursol, epaisseur);
+    const plancherGeo = new THREE.BoxGeometry(largeurplan, epaisseur, longueursol);
     const plancherMaterial = new THREE.MeshBasicMaterial({ color: '#63340b'});
     const plancher = new THREE.Mesh(plancherGeo, plancherMaterial);
-    plancher.position.set(2.5, 4, 2.7);//1.35
+    plancher.position.set(0, 2.5, 0);//1.35
     scene.add(plancher)
 
     const fenlargeur = 1.5;
